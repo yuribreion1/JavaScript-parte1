54869f6af9195a8e8d4b3978e740245e7baa4d25
Yuri Breion <yuribreion@MacBook-Air-de-Yuri.local>
2017-11-07 20:36:32 -0200

    Remover paciente
    Nesta versão foi adicionada uma classe no CSS para suavizar a remoção do paciente e aplicação de Timeout no JS.

--- introducao-javascript/js/remover-paciente.js
+++ introducao-javascript/js/remover-paciente.js
@@ -1,6 +1,17 @@
+/*
+    Programa criado para remoção de linha da tabela. 
+    Listener permanece ouvindo os filhos da tabela
+
+    Adicionamos um timeout de meio segundo para remover o item da tabela
+*/
+
 var tabela = document.querySelector("tbody");
 
 tabela.addEventListener("dblclick", function (event) {
-    event.target.parentNode.remove();
+
+    event.target.parentNode.classList.add("fadeOut")
+    setTimeout(function() {
+        event.target.parentNode.remove();
+    }, 500);
 })
 
