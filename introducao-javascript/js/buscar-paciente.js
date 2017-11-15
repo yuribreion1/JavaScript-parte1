65807cf8c43d055b329bb752477d97131409cc27
Yuri Breion <yuribreion@MacBook-Air-de-Yuri.local>
2017-11-14 23:10:47 -0200

    Validação de requisição a servidor
    Valida se tem response status igual a 200

--- introducao-javascript/js/buscar-paciente.js
+++ introducao-javascript/js/buscar-paciente.js
@@ -5,15 +5,21 @@ botaoBuscar.addEventListener("click", function () {
     console.log("Requisitando dados da API...")
 
     var xhr = new XMLHttpRequest();
+    var erroAjax = document.querySelector("#erro-ajax");
     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
 
     xhr.addEventListener("load", function () {
-        var resposta = xhr.responseText;
+        if (xhr.status == 200) {
+            erroAjax.classList.add("invisivel");
+            var resposta = xhr.responseText;
 
-        var pacientes = JSON.parse(resposta);
-        pacientes.forEach(function(paciente) {
-            adicionaPacienteNaTabela(paciente);
-        });
+            var pacientes = JSON.parse(resposta);
+            pacientes.forEach(function(paciente) {
+                adicionaPacienteNaTabela(paciente);
+            });
+        } else {
+            erroAjax.classList.remove("invisivel");
+        }
     });
     xhr.send();
 })
